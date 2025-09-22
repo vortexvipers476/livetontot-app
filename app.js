95c582f0290672cac912eea518966725e26fcf73
vortexvipers476 <vortexvipers1003@gmail.com>
2025-09-22 14:42:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 // Komponen utama aplikasi
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Nonton Bareng</title>
+        <meta name="description" content="Platform nonton bareng real-time dengan teman-teman" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
